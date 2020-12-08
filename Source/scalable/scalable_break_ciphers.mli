10870f4009e16d8b3da79919cca1cdba0af1a354
hugo.girard <hugo.girard@epita.fr>
2020-12-09 00:42:05 +0100

    supp: tous ce qui n'est pas ml

--- Source/scalable/scalable_break_ciphers.mli
+++
@@ -1,7 +0,0 @@
-(** Factoring bitarrays into primes *)
-
-(** Factors product of two prime bitarrays.
-    @param key is public key of an RSA cryptosystem.
- *)
-val break : int list * 'a -> ((int list) * (int list))
-;;
